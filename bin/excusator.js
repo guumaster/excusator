fd1d067cf9fe9b9be6e627a4efb6d1d8ece9b8d2
guumaster <gustavoandresmarin@gmail.com>
2014-10-01 13:00:22 +0200

    agregado parametro -s para reproducir sonido

--- bin/excusator.js
+++ bin/excusator.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
 var excusame = require('../lib');
+var argv = require('minimist')(process.argv.slice(2));
 
-console.log( excusame() );
+console.log( (argv.s || argv.speak ) ? excusame.say() : excusame.excusa() );
